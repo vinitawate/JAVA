f78b21cabb9b1f765fd851486b36528d0a3c8173
vinitawate <146056800+vinitawate@users.noreply.github.com>
2023-09-26 10:57:42 +0530

    Add files via upload

---
+++ TypeCast.java
@@ -0,0 +1,11 @@
+public class TypeCast{
+public static void  main(String[]args)
+{
+int x=7;
+long y=x;
+float z=y;
+System.out.println("Before Conversion,int value"+x);
+System.out.println("After Conversion,long value"+y);
+System.out.println("After Conversion,float value"+z);
+}
+}
---
+++ typecastnarrowing.java
@@ -0,0 +1,10 @@
+public class typecastnarrowing{
+public static void main(String[]args){
+double d=166.6;
+long l=(long)d;
+int i=(int)l;
+System.out.println("Before Conversion:"+d);
+System.out.println("After Conversion into long type:"+l);
+System.out.println("After Conversion into integer type:"+i);
+}
+}
